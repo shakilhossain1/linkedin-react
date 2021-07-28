ec1f263ef6c1136a6d7a4759ddf0079dc6a2c478
Shakil Hossain <sh9743441@gmail.com>
2021-07-28 18:08:24 +0600

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import Header from './components/Header';
+import Feed from './components/Feed';
 import Sidebar from './components/Sidebar';
 import tw from 'twin.macro';
 
-const AppBody = tw.main`flex`;
+const AppBody = tw.main`flex w-full max-w-7xl mx-auto`;
 
 export default function App() {
   return (
-    <div>
+    <div className="bg-gray-100 min-h-screen w-full flex flex-col items-center">
       <Header />
       <AppBody>
         <Sidebar />
+        <Feed />
       </AppBody>
     </div>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,39 +1,54 @@
 import { Avatar } from '@material-ui/core';
-import styled from 'styled-components';
 import tw from 'twin.macro';
 
-const Wrapper =styled.div`
-  flex: 0.2;
-  ${tw`sticky top-20 bg-gray-200`}
-`;
+const Wrapper = tw.div`sticky top-20 flex-grow-[0.2] rounded-lg text-center`;
+const SidebarTop = tw.div`flex flex-col items-center border border-gray-300 border-b-0 rounded-tl-lg rounded-tr-lg bg-white pb-3 overflow-hidden`;
+const SidebartopBackground = tw.img`-mb-4 w-full h-20 object-cover`;
+const StatParagraph = tw.p`text-gray-500 text-sm font-semibold`;
+const StatNumber = tw(StatParagraph)`font-bold text-blue-500`;
+const Stat = tw.div`mt-3 flex justify-between`;
+const SidebarBottom = tw.div`text-left p-3 border border-gray-300 rounded-xl mt-3 bg-white`;
+
+const RecentItem = tw.a`flex text-sm text-gray-600 font-extrabold mb-1 p-1 hover:bg-[whitesmoke] hover:rounded-xl hover:text-black`;
 
 export default function Sidebar() {
+  const recentItem = topic => (
+    <RecentItem href="#">
+      <span className="mr-2 ml-1">#</span>
+      <p>{topic}</p>
+    </RecentItem>
+  );
+
   return (
     <Wrapper>
-      {/* top */}
-      <div>
-        <img src="" alt="background" />
-        <Avatar src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?cs=srgb&dl=pexels-andrea-piacquadio-774909.jpg&fm=jpg" />
-        <h2>User name</h2>
-        <h2>example@example.com</h2>
-      </div>
+      <SidebarTop>
+        <SidebartopBackground src="https://images.unsplash.com/32/Mc8kW4x9Q3aRR3RkP5Im_IMG_4417.jpg?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" />
+        <Avatar
+          className="mb-3"
+          src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?cs=srgb&dl=pexels-andrea-piacquadio-774909.jpg&fm=jpg"
+        />
+        <h2 className="text-lg font-bold">User name</h2>
+        <h4 className="text-gray-500 text-sm">example@example.com</h4>
+      </SidebarTop>
       {/* sidebar stats */}
-      <div>
-        <div>
-          <p>Who viewd you</p>
-          <p>2.5</p>
-        </div>
-        <div>
-          <p>Who viewd you</p>
-          <p>2.5</p>
-        </div>
+      <div className="p-3 mb-3 border border-gray-300 bg-white rounded-bl-lg rounded-br-lg">
+        <Stat>
+          <StatParagraph>Who viewd you</StatParagraph>
+          <StatNumber>2.5</StatNumber>
+        </Stat>
+        <Stat>
+          <StatParagraph>Who viewd you</StatParagraph>
+          <StatNumber>2.5</StatNumber>
+        </Stat>
       </div>
 
-      {/* sidebar bottom */}
-      <div>
-        <p>Recent</p>
-
-      </div>
+      <SidebarBottom>
+        <p className="text-sm pb-3 font-bold">Recent</p>
+        {recentItem('reactjs')}
+        {recentItem('programing')}
+        {recentItem('php')}
+        {recentItem('laravel')}
+      </SidebarBottom>
     </Wrapper>
   );
 }
